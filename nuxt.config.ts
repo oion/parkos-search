59b61f882a0565cd86d66454a138a4074166a478
Ovidiu Ion <ovidiu.ion@extendio.ro>
2025-04-14 21:28:28 +0300

    SSR false

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   modules: ["@nuxt/eslint", "@nuxt/test-utils", "@nuxtjs/tailwindcss"],
+  ssr: false,
   tailwindcss: {
     editorSupport: true,
     exposeConfig: true,
